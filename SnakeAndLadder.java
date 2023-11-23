eb98890bc213f15240a2062de071623c50958404
unknown <dhanupriasokan@gmail.com>
2023-11-23 23:27:38 +0530

    usecase2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,15 +9,37 @@ public class SnakeAndLadder {
     public static ArrayList<Integer> ladder_end=new ArrayList<Integer>(Arrays.asList(38,14,31,42,85,67,91,99));
     public static ArrayList<Integer> snake_head=new ArrayList<Integer>(Arrays.asList(17,54,62,64,87,92,95,98));
     public static ArrayList<Integer> snake_tail=new ArrayList<Integer>(Arrays.asList(7,19,34,36,60,73,75,79));
-    
- 
+
+
+    public static int randomgenerate(){
+        int play= (int) ((Math.random()*10)%6);
+
+        return (play==0)?1:play;
+
+
+
+    }
+
+
+
+
+
 
     public static void main(String[] args){
         int player1Position=0;
-    
-        
+       
 
-  
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
